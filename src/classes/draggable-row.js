f61b88ed544eacce48cd92ba9d2a826519119913
Alex_Bartfeld <Welcome1>
2020-01-23 13:31:37 +0200

    small naming refactor

--- src/classes/draggable-row.js
+++ src/classes/draggable-row.js
@@ -2,21 +2,21 @@ import {getStyleVal} from "../utils/utils";
 
 export default class RowWrapper {
     constructor(props) {
-        this.startY = props.startY;
-        this.domNode = props.domNode;
-        this.topRelativePosition = parseInt(getStyleVal(this.domNode, 'top'));
-        this.domNode.style.opacity = '0.6';
+        this._startY = props.startY;
+        this._domNode = props.domNode;
+        this._topRelativePosition = parseInt(getStyleVal(this._domNode, 'top'));
+        this._domNode.style.opacity = '0.6';
         this._followTheMouse = this._followTheMouse.bind(this);
         window.addEventListener('mousemove', this._followTheMouse);
     }
 
     _followTheMouse(event) {
-        let newTop = this.topRelativePosition + (event.pageY - this.startY);
-        this.domNode.style.top = newTop + 'px';
+        let newTop = this._topRelativePosition + (event.pageY - this._startY);
+        this._domNode.style.top = newTop + 'px';
     }
 
     reset() {
-        this.domNode.style.opacity = '1';
+        this._domNode.style.opacity = '1';
         window.removeEventListener('mousemove', this._followTheMouse);
     }
 }
